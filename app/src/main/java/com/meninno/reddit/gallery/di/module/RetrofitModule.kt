bf72804947549bee3a162974dca26f01b6e7da73
Michele Meninno <michele.meninno@gmail.com>
2020-09-16 21:09:37 +0200

    - initial structuring

---
+++ app/src/main/java/com/meninno/reddit/gallery/di/module/RetrofitModule.kt
@@ -0,0 +1,39 @@
+package com.meninno.reddit.gallery.di.module
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(baseUrl: String, okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggerInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { this.level = HttpLoggingInterceptor.Level.BODY }
+    }
+
+
+}
---
+++ app/src/main/java/com/meninno/reddit/gallery/di/scopes/Scopes.kt
@@ -0,0 +1,7 @@
+package com.meninno.reddit.gallery.di.scopes
+
+import javax.inject.Scope
+@MustBeDocumented
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
